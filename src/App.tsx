928ab4d810ff4d5bfdd9d4ff4143a951da255059
tiagostmg <tiago.taumaturgo@gmail.com>
2025-04-21 13:09:25 -0300

    Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-// import { styles } from './App.module.css'
-import { Header } from './components/Header'
+import styles from './App.module.css'
+import { Header } from './components/Header.tsx'
+import './global.css'
 
 export function App() {
 
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        
+      </div>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,31 @@
+import styles from './Header.module.css'; 
+import logo from '../assets/react.svg'; 
+
 export function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       
-      <h1>My App</h1>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="Logo da Target" className={styles.logo} />
+        <span className={styles.companyName}>Target</span>
+      </div>
 
-
+      <nav className={styles.navigation}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <a href="/#" className={styles.navLink}>Home</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="/#" className={styles.navLink}>Sobre</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="/#" className={styles.navLink}>Serviços</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="/#" className={styles.navLink}>Contato</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
