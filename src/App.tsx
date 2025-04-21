2aa743f46ae3cbec2f1b0600870a26c84666967e
tiagostmg <tiago.taumaturgo@gmail.com>
2025-04-21 11:14:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { styles } from './App.module.css'
+import { Header } from './components/Header'
+
+export function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header>
+      
+      <h1>My App</h1>
+
+
+    </header>
+  )
+}
